9709a3be68955d019fa2c9d5d30918732df2f536
Lewis Guignard <lewisguignard@gmail.com>
2018-12-01 09:41:41 -0800

    add and subtractfunctions

---
+++ calculator.py
@@ -0,0 +1,19 @@
+"""
+Calculator library with basic math opps
+"""
+
+
+def add(first, second):
+    """TODO: Docstring for add.
+    :returns: TODO
+
+    """
+    return first + second
+
+
+def subtract(first, second):
+    """TODO: Docstring for subtract.
+    :returns: TODO
+
+    """
+    return first - second
